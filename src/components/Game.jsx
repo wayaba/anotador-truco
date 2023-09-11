a0f8b0b949876cef5d2ca44dc9979cc69167e821
unknown <wensel84@gmail.com>
2023-09-11 09:56:38 -0300

    cambio iconos

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,7 +7,7 @@ import { Modal } from './Modal'
 export const Game = () => {
   const [points, setPoints] = useState(getPoints)
   const [showModal, setShowModal] = useState(false)
-  const [modalMessage, setModalMessage] = useState({title:"", subtitle:""})
+  const [modalMessage, setModalMessage] = useState({ title: '', subtitle: '' })
 
   const handleWeOnChange = (value) => {
     if (!(value >= 0 && value <= 30)) return
@@ -24,7 +24,7 @@ export const Game = () => {
       we: points.we,
       they: value
     }
-   
+
     setCurrentPoints(newPointsObject, false, value)
   }
 
@@ -32,23 +32,22 @@ export const Game = () => {
     updatePoints(newPointsObject)
     setPoints(newPointsObject)
 
-    if(value === 30){
-      setTimeout(()=>{
+    if (value === 30) {
+      setTimeout(() => {
         setModalMessage({
-          title:`${isWe?'Nosotros ganamos':'Ellos ganaron'} el partido 🙂`,
-          subtitle: "¿Jugamos de nuevo?"
+          title: `${isWe ? 'Nosotros ganamos' : 'Ellos ganaron'} el partido 🙂`,
+          subtitle: '¿Jugamos de nuevo?'
         })
         setShowModal(true)
         //confetti()
-      },500)
-      
+      }, 500)
     }
   }
 
   const openModalRestart = () => {
     setModalMessage({
-      title:`Reiniciar el juego.  🚨`,
-      subtitle: "¿Estás seguro?"
+      title: `Reiniciar el juego.  🚨`,
+      subtitle: '¿Estás seguro?'
     })
     setShowModal(true)
   }
@@ -60,7 +59,7 @@ export const Game = () => {
     }
     updatePoints(newValue)
     setPoints(newValue)
-    
+
     setShowModal(false)
   }
   return (
@@ -85,15 +84,23 @@ export const Game = () => {
           subtitle={modalMessage.subtitle}
         />
       )}
-      <div className="w-1/3 m-auto">
-      <button
-        type="button"
-        onClick={openModalRestart}
-        className="px-4 py-2 my-0 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-      >
-        Reiniciar
-      </button>
-      </div>      
+      <div className="flex flex-row">
+        <img
+          src="./img/firulete.png"
+          className="w-28 h-10 mirrored-image mr-1"
+        ></img>
+        <button
+          type="button"
+          onClick={openModalRestart}
+          className="px-4 py-2 my-0 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+        >
+          Reiniciar
+        </button>
+        <img
+          src="./img/firulete.png"
+          className="aspect-auto w-28 h-10 ml-1 "
+        ></img>
+      </div>
     </section>
   )
 }
